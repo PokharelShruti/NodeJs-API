c5cc1a08317d204ef7ea96e792e144d89879e7a1
pokharelshruti <shrutipokharel26@gmail.com>
2025-05-19 17:53:09 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express= require('express');
+const app= express();
+
+app.use(express.json());
+let products=[
+    {id:1, name:'Laptop', price: 1000},
+    {id:2, name:'Keyboard', price:50}
+];
+app.get('/', (req,res)=>{
+    res.send('Welcome to ERP API');
+});
+app.get('/products',(req,res)=>{
+    res.json(products);
+});
+app.post('/products',(req,res)=>{
+    const newProduct= req.body;
+    products.push(newProduct);
+    res.status(201).json(newProduct);
+});
+app.put('/products/:id', (req,res)=>{
+const id=parseInt(req.params.id);
+const updatedProduct=req.body;
+
+});
+app.listen(3000,()=>{
+    console.log('Server running on http://localhost:3000');
+});
